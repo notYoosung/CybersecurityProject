570a0576ad22c9b20288c9fa46cf482f7ba1f715
notYoosung <98567880+notYoosung@users.noreply.github.com>
2024-11-06 13:03:24 -0600

    encoding/encrypting bugfixes

--- PGM_TEAM.m
+++ PGM_TEAM.m
@@ -1,124 +1,126 @@
 %{
-Title: PGM_TEAM
-Authors: Asa Fowler, Bryan Le
-Date: October 27, 2024
-Summary: 
+      Title:    PGM_TEAM
+    Authors:    Asa Fowler, Bryan Le
+       Date:    October 27, 2024
+    Summary:    
 %}
 
-function [] = PGM_TEAM()
+function[Sheet] = PGM_TEAM()
 
-    clear;
-    clc;
-    pkg load io;
-    %[num, txt, raw]
-    %[_, Headers, _] = xlsread('Cybersecurity.xlsx', 'Sheet1', 'A1:A6')
-    %txt(1:1,2:3)
-    %[_, Total, TotalRaw] = xlsread('Cybersecurity.xlsx', 'Sheet1', "")
+clear;
+clc;
+pkg load io;
+%[num, txt, raw]
+%[_, Headers, _] = xlsread('Cybersecurity.xlsx', 'Sheet1', 'A1:A6')
+%txt(1:1,2:3)
+%[_, Total, TotalRaw] = xlsread('Cybersecurity.xlsx', 'Sheet1', "")
 
-    Sheet = {...
-    'Patient',       'LUKE SKYWALKER', 'LEIA ORGANA', 'HAN SOLO';
-    'Gender',        'Male',           'Female',      'Male';
-    'DOB',           '1965-11-05',     '1973-10-13',  '1965-12-15';
-    'Children',      '2',              '0',           '1';
-    'Allergies',     'Grass, Mold',    'None',        'Carbonite, Wookie dander';
-    'Prescriptions', 'Zocor, Daforce', 'None',        'Cymbalta';
-    };
-    %xlswrite('Cybersecurity.xlsx', Sheet);
 
-    SheetEncoded = {'Patient'; 'Gender'; 'DOB'; 'Children'; 'Allergies'; 'Prescriptions'};
-    SheetEncrypt = {'Patient'; 'Gender'; 'DOB'; 'Children'; 'Allergies'; 'Prescriptions'};
+Sheet = {...
+  'Patient',       'LUKE SKYWALKER', 'LEIA ORGANA', 'HAN SOLO';
+  'Gender',        'Male',           'Female',      'Male';
+  'DOB',           '1965-11-05',     '1973-10-13',  '1965-12-15';
+  'Children',      '2',              '0',           '1';
+  'Allergies',     'Grass, Mold',    'None',        'Carbonite, Wookie dander';
+  'Prescriptions', 'Zocor, Daforce', 'None',        'Cymbalta';
+};
+xlswrite('Cybersecurity.xlsx', Sheet);
 
-    dataamt = size(Sheet, 1);
+SheetEncoded = { 'Patient'; 'Gender'; 'DOB'; 'Children'; 'Allergies'; 'Prescriptions' };
 
-    function [] = WriteEncoded(Cell)
-        [encoded, keys] = Encoder_TEAM(Sheet(:, Cell)');
-        Sheet((1 + dataamt):(dataamt*2), Cell) = encoded;
-        Sheet((dataamt*2 + 1):(2 * dataamt), Cell) = keys;
+dataamt = size(Sheet, 1);
+
+for i = 2:(size(Sheet, 2)-2)
+    [encoded, keys] = Encoder_TEAM(Sheet(:, i)');
+    SheetEncoded(1:dataamt, i) = encoded;
+    SheetEncoded((dataamt+1):(2*dataamt), i) = keys;
+
+endfor
+
+    function [] = WriteEncoded(Cell, Col)
+        [encoded, keys] = Encoder_TEAM(Cell);
+        Sheet((    dataamt + 1):(2 * dataamt), Col) = encoded;
+        Sheet((2 * dataamt + 1):(3 * dataamt), Col) = keys;
     endfunction
 
     function Cell = ReadEncoded(ColumnN)
         Cell = Decoder_TEAM(Sheet((dataamt+1):(dataamt*2), ColumnN)', Sheet((dataamt + 1):(2 * dataamt), ColumnN)');
     endfunction
 
-    function [] = WriteEncrypted(ColumnN)
-        [encrypted, keys] = Encoder_TEAM(Sheet(:, ColumnN)');
-        disp(encrypted(1, 1:dataamt))
-        Sheet{1:dataamt * 4, ColumnN} = [Sheet(:, ColumnN) repelem('', dataamt * 4 - size(Sheet(:, ColumnN), 2))]
-        Sheet((dataamt * 2 + 1):(dataamt * 3), ColumnN) = encrypted(1:dataamt, 1);
-        Sheet((dataamt*3 + 1):(4 * dataamt), ColumnN) = keys;
+    function [] = WriteEncrypted(Cell, ColumnN)
+        [encrypted, keys] = Encoder_TEAM(Cell);
+        %Sheet{1:dataamt * 4, ColumnN} = [Sheet(:, ColumnN) repelem('', dataamt * 4 - size(Sheet(:, ColumnN), 2))]
+        Sheet((3 * dataamt + 1):(4 * dataamt), ColumnN) = encrypted(1, 1:dataamt);
+        Sheet((4 * dataamt + 1):(5 * dataamt), ColumnN) = keys;
     endfunction
 
     function Cell = ReadEncrypted(ColumnN)
         Cell = Decrypt_TEAM(Sheet((dataamt * 2 + 1):(dataamt * 3), ColumnN)', Sheet((dataamt * 3 + 1):(4 * dataamt), ColumnN)');
     endfunction
-
-    for i = 2:(size(Sheet, 2) - 2)
-        WriteEncoded(i)
-        % WriteEncrypted(i)
-    endfor
     
+    for i = 2:(size(Sheet, 2) - 2)
+        WriteEncoded(Sheet(1:dataamt, i)', i)
+        WriteEncrypted(Sheet(1:dataamt, i)', i)
+    endfor
 
-    % xlswrite('Cybersecurity.xlsx', Sheet);
+
+%m = ReadEncoded(2)
+
+
+%Crete a new patient or read patient data
+
+
+% function[] = PGM_TEAM()
+Option = questdlg('Create or read patient data? ', 'Menu', 'Create', 'Read', 'Read');
+Option = 'Create'
+switch Option
+  case 'Create'
+
+    prompts = {'Patient' 'Gender' 'DOB' 'Children' 'Allergies' 'Prescriptions'};
+    PatientData = inputdlg(prompts, 'Create Patient');
+    PatientData = {'Patient' 'Gender' 'DOB' 'Children' 'Allergies' 'Prescriptions'};
+    % xlswrite('Patients.xlsx', );
+    % xlswrite('Patients.xlsx', 'Encoded', );
+    [EncodedCell, RotationCell] = Encoder_TEAM(PatientData)
+    % xlswrite('Cybersecurity.xlsx', EncodedCell, 'Encoded (A)');
+    WriteEncoded(EncodedCell, size(Sheet, 2))
     disp(Sheet)
-
-    m = ReadEncoded(2)
-    % m = ReadEncrypted(2)
-
-    %Crete a new patient or read patient data
-
-    % function[] = PGM_TEAM()
-    Option = questdlg('Create or read patient data? ', 'Menu', 'Create', 'Read', 'Read');
-
-    switch Option
-        case 'Create'
-
-            prompts = {'Patient' 'Gender' 'DOB' 'Children' 'Allergies' 'Prescriptions'};
-            PatientData = inputdlg(prompts, 'Create Patient');
-            % xlswrite('Patients.xlsx', );
-            % xlswrite('Patients.xlsx', 'Encoded', );
-            EncodedCell = Encoder_TEAM(PatientData)
-            % xlswrite('Cybersecurity.xlsx', EncodedCell, 'Encoded (A)');
-            Sheet{1, :} = EncodedCell
-
-        case 'Read'
-            d = dir;
-            patients = Sheet(1, 2:size(Sheet, 2));
-            [indx, isSelected] = listdlg('Name', 'Patient Selection', ...
-                'PromptString', {'Select a patient.'}, ...
-                'SelectionMode', 'single', ...
-                'ListString', patients);
-
-            if isSelected
-
-                for i = 2:size(Sheet, 2)
-
-                    if indx[1] == i + 1
-                        fprintf('Patient Data:\n')
-
-                        for j = 1:dataamt
-                            fprintf('\t%10s:\t %s\n', char(Sheet(j, 1)), char(Sheet(j, i)))
-                        endfor
-
-                        break
-                    endif
-
+  case 'Read'
+    d = dir;
+    patients = Sheet(2:size(Sheet, 1), 1);
+    [indx, isSelected] = listdlg('Name', 'Patient Selection', ...
+        'PromptString', {'Select a patient.'}, ...
+        'SelectionMode', 'single', ...
+        'ListString', patients);
+    if isSelected
+        for i = 2:size(Sheet, 1)
+            if indx[1] == i+1
+                fprintf('Patient Data:\n')
+                for j = 1:size(Sheet, 2)
+                    fprintf('\t%10s:\t %s\n', char(Sheet(1, j)), char(Sheet(i, j)))
                 endfor
-
-            else
-                fprintf('exited')
+                break
             endif
+        endfor
+    else
+        fprintf('exited')
+    endif
+  otherwise
+    exitChoice = input('Quit/Restart?', 's')
+    %exitChoice = questdlg('Quit/Restart?', 'End of Program', 'Quit', 'Restart', 'Quit');
 
-        otherwise
-            exitChoice = input('Exit? (Yes/No)', 's')
-            %exitChoice = questdlg('Exit program? ', 'End', 'Yes', 'No', 'Yes');
-
-            if exitChoice == 'No'
-                PGM_TEAM()
-            endif
-
+    switch exitChoice
+        case 'Quit'
+            fprintf('Thank')
+        case 'Restart'
+            PGM_TEAM()
     endswitch
+    
+endswitch
+
 
 endfunction
+
 
 %CybersecurityA('abc', 1)
 
